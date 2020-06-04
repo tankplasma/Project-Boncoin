82fb1a213bba4e2dfe0c56235f0a662bbec99bf2
tankplasma <adrien.alard@gmail.com>
2020-06-04 15:17:48 +0200

    first commit

---
+++ login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="check.php">
+        <div>
+            <label>Login: <input type="text" name="login"></label>
+        </div>
+        <div>
+            <label>Password: <input type="password" name="pass"></label>
+        </div>
+        <div>
+            <input type="submit" value="Envoyer">
+        </div>
+</body>
+</html>
